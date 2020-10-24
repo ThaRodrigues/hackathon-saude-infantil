24d37d009d9ab87b6aefdeee15817615209e01ab
luanfeo <luanfeo@gmail.com>
2020-10-24 17:00:40 -0300

    commit inicial

---
+++ src/main/groovy/com/hackthon/obsidadeinfantill/ObsidadeinfantillApplication.groovy
@@ -0,0 +1,13 @@
+package com.hackthon.obsidadeinfantill
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ObsidadeinfantillApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ObsidadeinfantillApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/hackthon/obsidadeinfantill/ObsidadeinfantillApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.hackthon.obsidadeinfantill
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObsidadeinfantillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
