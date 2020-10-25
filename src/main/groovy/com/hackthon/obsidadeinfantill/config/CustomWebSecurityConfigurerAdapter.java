fae51ca385cc0a61e8df127fb57a039878ffee89
luanfeo <luanfeo@gmail.com>
2020-10-25 12:22:33 -0300

    Retirada das configurações do cors

--- src/main/groovy/com/hackthon/obsidadeinfantill/config/CustomWebSecurityConfigurerAdapter.java
+++ src/main/groovy/com/hackthon/obsidadeinfantill/config/CustomWebSecurityConfigurerAdapter.java
@@ -1,16 +1,7 @@
 package com.hackthon.obsidadeinfantill.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
-@Configuration
-public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+public class CustomWebSecurityConfigurerAdapter   {
 
-    }
 }
