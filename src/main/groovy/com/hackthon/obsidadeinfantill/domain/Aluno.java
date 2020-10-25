89a8d18a3515b997043f741329b8b3dee4fbd479
luanfeo <luanfeo@gmail.com>
2020-10-25 01:17:52 -0300

    setter e getters faltantes

--- src/main/groovy/com/hackthon/obsidadeinfantill/domain/Aluno.java
+++ src/main/groovy/com/hackthon/obsidadeinfantill/domain/Aluno.java
@@ -46,4 +46,12 @@ public class Aluno {
     public void setPeso(BigDecimal peso) {
         this.peso = peso;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
